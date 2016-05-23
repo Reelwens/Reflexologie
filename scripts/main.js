93b62e1276aa27aa73559340996ac62571173cfc
Charlotte Farando <charlotte.farando@gmail.com>
2016-05-23 18:33:29 +0200

    Menu charlotte

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+document.write("OUT");
+
+// commentaire sur java script 
+
+/*
+Commentaire sur pleusieurs lignes 
+
+*/
+
+console.log("Hello World")
+
+var x=2;
+console.log(x); 
+
+window.alert("Ceci est une erreur");
+var age = window.prompt("Quel est votre age ?", "18");
+console.log(age);
+
+var majeur = window.confirm("Pour consulter ce site, vous devez être maheur");
+console.log(majeur);
+
+// EX 1
+var texte = "Hello World";
+console.log(helloWorld);
+
+//EX 2
+var entier=12
+console.log(12)
+
+// EX 3
+var mat="L'été sera chaud !";
+console.log(mat);
