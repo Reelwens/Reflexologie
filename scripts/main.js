dc6f4bc0620168c9571950c25e1a76c291123a3d
alison24 <alison.heredia@hetic.net>
2016-05-30 13:32:27 +0200

    la page

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,101 @@
-document.write("OUT");
 
-// commentaire sur java script 
+$(function() {
+	/** -----------------------------------------
+	 Slider 
+	 -------------------------------------------*/
+	 var SliderModule = (function() {
+	 	var pb = {};
+	 	pb.el = $('#slider');
+	 	pb.items = {
+	 		panels: pb.el.find('.slider-wrapper > li'),
+	 	}
 
-/*
-Commentaire sur pleusieurs lignes 
+	 	// Interval de Slider
+	 	var SliderInterval,
+	 		currentSlider = 0,
+	 		nextSlider = 1,
+	 		lengthSlider = pb.items.panels.length;
 
-*/
+	 	// Construction Slider
+	 	pb.init = function(settings) {
+	 		this.settings = settings || {duration: 8000};
+	 		var items = this.items,
+	 			lengthPanels = items.panels.length,
+	 			output = '';
 
-console.log("Hello World")
+	 		
+	 		for(var i = 0; i < lengthPanels; i++) {
+	 			if(i == 0) {
+	 				output += '<li class="active"></li>';
+	 			} else {
+	 				output += '<li></li>';
+	 			}
+	 		}
 
-var x=2;
-console.log(x); 
+	 		$('#control-buttons').html(output);
 
-window.alert("Ceci est une erreur");
-var age = window.prompt("Quel est votre age ?", "18");
-console.log(age);
+	 		// Activation Slider
+	 		activateSlider();
+	 		// evenement controle
+	 		$('#control-buttons').on('click', 'li', function(e) {
+	 			var $this = $(this);
+	 			if(!(currentSlider === $this.index())) {
+	 				changePanel($this.index());
+	 			}
+	 		});
 
-var majeur = window.confirm("Pour consulter ce site, vous devez être maheur");
-console.log(majeur);
+	 	}
 
-// EX 1
-var texte = "Hello World";
-console.log(helloWorld);
+	 	// Funcion pour activer le slider
+	 	var activateSlider = function() {
+	 		SliderInterval = setInterval(pb.startSlider, pb.settings.duration);
+	 	}
 
-//EX 2
-var entier=12
-console.log(12)
+	 	// Funcion pour l animation
+	 	pb.startSlider = function() {
+	 		var items = pb.items,
+	 			controls = $('#control-buttons li');
+	 		// retour au debut 
+	 		if(nextSlider >= lengthSlider) {
+	 			nextSlider = 0;
+	 			currentSlider = lengthSlider-1;
+	 		}
 
-// EX 3
-var mat="L'été sera chaud !";
-console.log(mat);
+	 		controls.removeClass('active').eq(nextSlider).addClass('active');
+	 		items.panels.eq(currentSlider).fadeOut('slow');
+	 		items.panels.eq(nextSlider).fadeIn('slow');
+
+	 		
+	 		currentSlider = nextSlider;
+	 		nextSlider += 1;
+	 	}
+
+	 	
+	 	var changePanel = function(id) {
+	 		clearInterval(SliderInterval);
+	 		var items = pb.items,
+	 			controls = $('#control-buttons li');
+	 		
+	 		if(id >= lengthSlider) {
+	 			id = 0;
+	 		} else if(id < 0) {
+	 			id = lengthSlider-1;
+	 		}
+
+	 		controls.removeClass('active').eq(id).addClass('active');
+	 		items.panels.eq(currentSlider).fadeOut('slow');
+	 		items.panels.eq(id).fadeIn('slow');
+
+	 		
+	 		currentSlider = id;
+	 		nextSlider = id+1;
+	 		
+	 		activateSlider();
+	 	}
+
+		return pb;
+	 }());
+
+	 SliderModule.init({duration: 4000});
+
+});
