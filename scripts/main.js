6e24acbb43544b4ec4d8d1558d631e6b5ac0683b
AXLR01 <raphael.camizuli01@gmail.com>
2016-05-31 15:27:11 +0200

    slider
    bg le slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,101 +1,31 @@
-
-$(function() {
-	/** -----------------------------------------
-	 Slider 
-	 -------------------------------------------*/
-	 var SliderModule = (function() {
-	 	var pb = {};
-	 	pb.el = $('#slider');
-	 	pb.items = {
-	 		panels: pb.el.find('.slider-wrapper > li'),
-	 	}
-
-	 	// Interval de Slider
-	 	var SliderInterval,
-	 		currentSlider = 0,
-	 		nextSlider = 1,
-	 		lengthSlider = pb.items.panels.length;
-
-	 	// Construction Slider
-	 	pb.init = function(settings) {
-	 		this.settings = settings || {duration: 8000};
-	 		var items = this.items,
-	 			lengthPanels = items.panels.length,
-	 			output = '';
-
-	 		
-	 		for(var i = 0; i < lengthPanels; i++) {
-	 			if(i == 0) {
-	 				output += '<li class="active"></li>';
-	 			} else {
-	 				output += '<li></li>';
-	 			}
-	 		}
-
-	 		$('#control-buttons').html(output);
-
-	 		// Activation Slider
-	 		activateSlider();
-	 		// evenement controle
-	 		$('#control-buttons').on('click', 'li', function(e) {
-	 			var $this = $(this);
-	 			if(!(currentSlider === $this.index())) {
-	 				changePanel($this.index());
-	 			}
-	 		});
-
-	 	}
-
-	 	// Funcion pour activer le slider
-	 	var activateSlider = function() {
-	 		SliderInterval = setInterval(pb.startSlider, pb.settings.duration);
-	 	}
-
-	 	// Funcion pour l animation
-	 	pb.startSlider = function() {
-	 		var items = pb.items,
-	 			controls = $('#control-buttons li');
-	 		// retour au debut 
-	 		if(nextSlider >= lengthSlider) {
-	 			nextSlider = 0;
-	 			currentSlider = lengthSlider-1;
-	 		}
-
-	 		controls.removeClass('active').eq(nextSlider).addClass('active');
-	 		items.panels.eq(currentSlider).fadeOut('slow');
-	 		items.panels.eq(nextSlider).fadeIn('slow');
-
-	 		
-	 		currentSlider = nextSlider;
-	 		nextSlider += 1;
-	 	}
-
-	 	
-	 	var changePanel = function(id) {
-	 		clearInterval(SliderInterval);
-	 		var items = pb.items,
-	 			controls = $('#control-buttons li');
-	 		
-	 		if(id >= lengthSlider) {
-	 			id = 0;
-	 		} else if(id < 0) {
-	 			id = lengthSlider-1;
-	 		}
-
-	 		controls.removeClass('active').eq(id).addClass('active');
-	 		items.panels.eq(currentSlider).fadeOut('slow');
-	 		items.panels.eq(id).fadeIn('slow');
-
-	 		
-	 		currentSlider = id;
-	 		nextSlider = id+1;
-	 		
-	 		activateSlider();
-	 	}
-
-		return pb;
-	 }());
-
-	 SliderModule.init({duration: 4000});
-
+$(document).ready(function(){
+	var percent = 0,
+	interval = 20,//it takes about 6s, interval=20 takes about 4s
+	$bar = $('.transition-timer-carousel-progress-bar'),
+	$crsl = $('#myCarousel');
+	$('.carousel-indicators li, .carousel-control').click(function (){$bar.css({width:0.5+'%'});});
+	/*line above just for showing when controls are clicked the bar goes to 0.5% to make more friendly, 
+	if you want when clicked set bar empty, change on width:0.5 to width:0*/
+	$crsl.carousel({//initialize
+		interval: false,
+		pause: true
+	}).on('slide.bs.carousel', function (){percent = 0;});//This event fires immediately when the bootstrap slide instance method is invoked.
+	function progressBarCarousel() {
+		$bar.css({width:percent+'%'});
+		percent = percent +0.5;
+		if (percent>=100) {
+			percent=0;
+			$crsl.carousel('next');
+		}
+	}
+	var barInterval = setInterval(progressBarCarousel, interval);//set interval to progressBarCarousel function
+	if (!(/Mobi/.test(navigator.userAgent))) {//tests if it isn't mobile
+		$crsl.hover(function(){
+					clearInterval(barInterval);
+				},
+				function(){
+					barInterval = setInterval(progressBarCarousel, interval);
+				}
+		);
+	}
 });
