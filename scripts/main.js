748099370ea5c1dd02d2704fec22a66d9d3c8c78
Simon <sim.lucas27@gmail.com>
2016-06-03 23:42:44 +0200

    Fix indentation

--- scripts/main.js
+++ scripts/main.js
@@ -105,22 +105,26 @@ $(document).ready(function () {
 
 });
 
-/************************************************************************************************************** apparition/disparition texte ***********************************************************************************************************************************/
+/***************************************
+****************************************
+-     APPARITION/DISPARITION TEXTE     -
+****************************************
+***************************************/
 
-  function affiche_contenu() {
-      var cible = document.getElementById('cible');
-      if(cible.style.display != '') {
-        cible.style.display = '';
-      } else {
-        cible.style.display = 'none';
-      }
-    }
+function affiche_contenu() {
+	var cible = document.getElementById('cible');
+	if(cible.style.display != '') {
+		cible.style.display = '';
+	} else {
+		cible.style.display = 'none';
+	}
+}
 
- function affiche_contenu1() {
-      var cible = document.getElementById('cible1');
-      if(cible.style.display != '') {
-        cible.style.display = '';
-      } else {
-        cible.style.display = 'none';
-      }
-    }
+function affiche_contenu1() {
+	var cible = document.getElementById('cible1');
+	if(cible.style.display != '') {
+		cible.style.display = '';
+	} else {
+		cible.style.display = 'none';
+	}
+}
